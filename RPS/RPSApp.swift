fcc287abf7ad9e8e99b8e3b24f4cbde38f9bd6be
Mitch-R-Code <mrichburg13@gmail.com>
2025-04-03 10:07:41 -0500

    Initial commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Admin on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPSTests/RPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTests.swift
+//  RPSTests
+//
+//  Created by Admin on 4/3/25.
+//
+
+import Testing
+@testable import RPS
+
+struct RPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
